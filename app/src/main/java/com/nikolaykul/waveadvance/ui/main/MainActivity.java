b04bf2bfb7249646178c92f85f45a5b1d85fadc1
Nikolay <gN1kolaykul@gmail.com>
2016-05-25 21:03:28 +0300

    Perform simple computation.

--- app/src/main/java/com/nikolaykul/waveadvance/ui/main/MainActivity.java
+++ app/src/main/java/com/nikolaykul/waveadvance/ui/main/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Pair;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.nikolaykul.waveadvance.R;
 import com.nikolaykul.waveadvance.databinding.ActivityMainBinding;
 import com.nikolaykul.waveadvance.di.component.ActivityComponent;
@@ -18,17 +19,37 @@ import javax.inject.Inject;
 public class MainActivity extends BaseActivity implements MainMvpView, OnTapListener {
     @Inject MainPresenter mPresenter;
     private ActivityMainBinding mBinding;
+    private MaterialDialog mStub;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mBinding.imageView.setOnTapListener(this);
         initToolbar(mBinding.toolbar);
+        mStub = new MaterialDialog.Builder(this)
+                .content(R.string.message_loading)
+                .cancelable(false)
+                .build();
         mPresenter.initWithView(this);
     }
 
     @Override protected void injectSelf(ActivityComponent component) {
         component.inject(this);
+    }
+
+    @Override public void showLoading() {
+        mStub.show();
+    }
+
+    @Override public void hideLoading() {
+        mStub.hide();
+    }
+
+    @Override public void showNewCoordinate(Pair<Double, Double> coordinate) {
+        final double x = coordinate.first;
+        final double y = coordinate.second;
+        final String msg = String.format(Locale.getDefault(), "X = %f, Y = %f", x, y);
+        mBinding.tvResult.setText(msg);
     }
 
     @Override public void onSingleTap(Pair<Float, Float> dot) {
@@ -37,6 +58,7 @@ public class MainActivity extends BaseActivity implements MainMvpView, OnTapList
 
     @Override public void onDoubleTap(Pair<Float, Float> dot) {
         displayCoordinates(dot.first, dot.second);
+        mPresenter.computeNewCoordinate(dot);
     }
 
     @Override protected void onDestroy() {
--- app/src/main/java/com/nikolaykul/waveadvance/ui/main/MainPresenter.java
+++ app/src/main/java/com/nikolaykul/waveadvance/ui/main/MainPresenter.java
@@ -1,9 +1,14 @@
 package com.nikolaykul.waveadvance.ui.main;
 
+import android.util.Pair;
+
 import com.nikolaykul.waveadvance.ui.base.Presenter;
 
 import javax.inject.Inject;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 import timber.log.Timber;
 
 public class MainPresenter extends Presenter<MainMvpView> {
@@ -20,4 +25,30 @@ public class MainPresenter extends Presenter<MainMvpView> {
     public void destroy() {
         Timber.i("MainPresenter was destroyed.");
     }
+
+    public void computeNewCoordinate(Pair<Float, Float> coordinate) {
+        final Observable<Double> obsX =
+                Observable.defer(() -> Observable.just(functionX(coordinate)));
+        final Observable<Double> obsY =
+                Observable.defer(() -> Observable.just(functionY(coordinate)));
+        Observable.combineLatest(obsX, obsY, Pair::new)
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(() -> getMvpView().showLoading())
+                .doAfterTerminate(() -> getMvpView().hideLoading())
+                .subscribe(getMvpView()::showNewCoordinate);
+    }
+
+    private double functionX(Pair<Float, Float> pair) {
+        final float x = pair.first;
+        final float y = pair.second;
+        return Math.abs(Math.exp(x) + Math.exp(y));
+    }
+
+    private double functionY(Pair<Float, Float> pair) {
+        final float x = pair.first;
+        final float y = pair.second;
+        return Math.abs(Math.sin(x) + Math.cos(y));
+    }
+
 }
